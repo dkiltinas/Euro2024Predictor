19bb7af95e68ad7ec0ff95f3020239dc5e78da87
dkiltinas <kiltinasdeividas@gmail.com>
2024-06-24 17:21:21 +0300

    Fixed the issue with the button

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -10,12 +10,12 @@ const Hero = ({ backgroundImage, title, subtitle, onClick }) => {
       }}
     >
       <div className="absolute inset-0 bg-customBlue opacity-50"></div>
-      <div className="relative z-10 text-center px-6">
+      <div className="relative text-center px-6">
         <h1 className="text-4xl md:text-5xl font-bold">{title}</h1>
         <p className="mt-4 text-lg md:text-xl">{subtitle}</p>
         <Link to={"/"}>
           <button
-            className="mt-24  sm:mt-52 px-32 py-3 relative  bg-yellow-500 text-black font-semibold rounded hover:bg-yellow-600 -z-2"
+            className="mt-24  sm:mt-52 px-32 py-3 relative  bg-yellow-500 text-black font-semibold rounded hover:bg-yellow-600 z-2"
             onClick={onClick}
           >
             Make a Guess!
